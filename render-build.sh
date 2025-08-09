dc66fd36fab2de4d270e32519a1def65f6f8c778
LUKESH PAWAR <157102729+LukeshPawar@users.noreply.github.com>
2025-08-09 22:28:07 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y \
-    cmake \
-    g++ \
-    make \
-    libboost-all-dev \
-    libopenblas-dev \
-    liblapack-dev
-pip install -r requirements.txt
